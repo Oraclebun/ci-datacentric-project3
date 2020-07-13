9ef0157fdb067dede9f58907ffade2749475d119
Mien Ling <mien_ling@hotmail.com>
2020-07-13 16:33:53 +0000

    add initial draft of ER diagram to provide visualization of entity relationship in documents folder, write up models of entity based on the draft of ER diagram in models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 from dotenv import load_dotenv
-from pymodm import connect, fields, MongoModel, EmbeddedDocumentListField
+from pymodm import connect, fields, MongoModel, EmbeddedMongoModel
 
 load_dotenv()
 
@@ -17,9 +17,10 @@ connect(MONGODB_URI)
 
 class Hiker(MongoModel):
     """
-    A model defining hiker, with hiker attribute, first name, last name, country of origin and e-mail.
-    Profile pic is a url field that stores the link to the hiker's profile photo.
-    Trails completed refers to the no of trails completed by hiker currently.
+    A model defining hiker, with hiker attribute, first name, last name, 
+    country of origin and e-mail. Profile pic is a url field that stores 
+    the link to the hiker's profile photo. Trails completed field refers 
+    to the no. of trails completed by hiker currently.
     """
     fname = fields.CharField(min_length=2, max_length=30)
     lname = fields.CharField(max_length=20)
@@ -32,3 +33,62 @@ class Hiker(MongoModel):
 
     def __str__(self):
         return f'{self.fname}({self.lname})'
+
+
+class Location(MongoModel):
+    """
+    A model defining location entity with embedded province and town 
+    information.
+    """
+    country = fields.CharField(max_length=30, required=True)
+    province = fields.EmbeddedDocumentListField('Province', required=True)
+
+
+class Province(EmbeddedMongoModel):
+    state = fields.CharField(max_length=30, required=True)
+    town = fields.EmbeddedDocumentField('Town', required=True)
+
+
+class Town(EmbeddedMongoModel):
+    town = fields.CharField(max_length=30, required=True)
+
+
+class Trails(MongoModel):
+    """
+    A model defining trails entity undertaken by hikers. It has embedded 
+    comments which reference the hiker.
+    """
+    trail_name = fields.CharField(max_length=100, required=True)
+    distance = fields.FloatField(max_value=50, required=True)
+    elevation = fields.FloatField(required=True)
+    route_type = fields.CharField(max_length=20, choices=(
+        'Point to Point', 'Loop', 'Out and Back'), required=True)
+    difficulty = fields.CharField(max_length=20, choices=(
+        'Easy', 'Moderate', 'Difficult'), required=True)
+    description = fields.CharField(max_length=500, required=True)
+    centrepoint = fields.PointField(required=False)
+    waypoints = fields.LineStringField(required=False)
+    embed_route = fields.CharField(max_length=800, required=True)
+    location = fields.ReferenceField(Location)
+    comments = fields.EmbeddedDocumentListField('Comment', required=False)
+
+    def __str__(self):
+        return self.trail_name
+
+
+class Comment(EmbeddedMongoModel):
+    """
+    A model that defines the reviews entity created by hikers. This 
+    model is embedded in each trail.
+    """
+    author = fields.ReferenceField(Hiker)
+    date_comment = fields.DateTimeField(default=datetime.datetime.now())
+    body = fields.CharField(max_length=250)
+    sightings = fields.ListField(fields.CharField(max_length=20))
+    ratings = fields.IntegerField(min_value=1, max_value=5)
+    date_started = fields.DateTimeField()
+    hours_taken = fields.IntegerField(min_value=0, max_value=48)
+    minutes_taken = fields.IntegerField(min_value=0, max_value=59)
+
+    def __str__(self):
+        return f'{self.author.fname}{self.author.lname}'
