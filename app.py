582efcea386719f17aa828afe689c2ae3c286fd8
Mien Ling <mien_ling@hotmail.com>
2020-07-14 06:24:31 +0000

    add card to index.html, create dictionary of all trail location and print out for debugging in app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 from models import Hiker, Trails, Location, Comment
 import os
+import pymongo
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    comments = Trails.objects.order_by(
+        [('comments.date_comment', pymongo.DESCENDING)])
     all_trails = Trails.objects.values().all()
     all_trail_loc = Trails.objects.only('location')
-    for t in all_trails:
+    trails_loc = []
+    for t in comments:
         print(t)
+    for loc in all_trail_loc:
+        locations = {
+            'trail_id': loc._id,
+            'trail_name': Trails.objects.get({'_id': loc._id}).trail_name,
+            'country': loc.location.country,
+            'province': loc.location.province[0].state,
+            'town': loc.location.province[0].town.town
+        }
+        trails_loc.append(locations)
+    print(trails_loc)
     return render_template('index.html')
 
 # "magic code" -- boilerplate
