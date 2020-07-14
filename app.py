d98081008b5317a7cbdeca0b5e0f1a467bb01dcf
Mien Ling <mien_ling@hotmail.com>
2020-07-14 10:18:55 +0000

    add for loop to retrieve comments from trails, pass it to index.html, loop the comments and display in index.html as cards.

--- app.py
+++ app.py
@@ -9,11 +9,23 @@ app = Flask(__name__)
 def index():
     comments = Trails.objects.order_by(
         [('comments.date_comment', pymongo.DESCENDING)])
+    all_comm=list(comments.only('comments'))
     all_trails = Trails.objects.values().all()
     all_trail_loc = Trails.objects.only('location')
     trails_loc = []
-    #for t in comments:
-    #    print(t)
+    reviews=[]
+    #for t in all_comm:
+    #    print(t.comments[0].author, t.comments[0].body)
+    for i in range(3):
+        comments={
+            'author': all_comm[i:(i+1)][0].comments[0].author.fname+' '+all_comm[i:(i+1)][0].comments[0].author.lname,
+            'photo' : all_comm[i:(i+1)][0].comments[0].author.profile_pic,
+            'body':all_comm[i:(i+1)][0].comments[0].body,
+            'date':all_comm[i:(i+1)][0].comments[0].date_comment,
+            'track':Trails.objects.get({'_id':all_comm[i:i+1][0]._id}),
+            'country':Trails.objects.get({'_id':all_comm[i:i+1][0]._id}).location.country
+        }
+        reviews.append(comments)
     for loc in all_trail_loc:
         locations = {
             'trail_id': loc._id,
@@ -23,7 +35,7 @@ def index():
             'town': loc.location.province[0].town.town
         }
         trails_loc.append(locations)
-    return render_template('index.html', location = trails_loc)
+    return render_template('index.html', location = trails_loc, comments=reviews)
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
