c156e6ddc8f573ddd371101454994c0e583343ff
Mien Ling <mien_ling@hotmail.com>
2020-07-14 16:44:32 +0000

    style up the comments in trails.template.html to place image on the left, show comment sightings in materialize chips/pill badges, write code to show rating stars as stars instead of rating numbers.

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
     //initialize collapsible (accordion)
     $('.collapsible').collapsible();
 
+    $.fn.stars = function () {
+        return $(this).each(function () {
+            var val = parseFloat($(this).html());       //value of rating
+            var size = Math.max(0, (Math.min(5, val))) * 24;    //find the max of the rating, multiply by 24
+            var $span = $('<span />').width(size);      //use the calculated size to size the span
+            $(this).html($span);                        //replace this with width of the span
+        });
+    }
+  
+    $(function() {
+        $('span.stars').stars();
+    });
+
   });// end document ready
